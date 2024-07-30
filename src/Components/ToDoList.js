c112f92cde87c1f09cc775ff3133b677c765fd86
Rabia-Rajput <rajput.rabia@outlook.com>
2024-07-30 22:25:29 +0200

    updated file

---
+++ src/Components/ToDoList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ToDoItem from "./ToDoItem";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import "../input.css";
+
+const ToDoList = ({ items, setItems }) => {
+  const handleClick = (Delete) => {
+    setItems((previtems) => previtems.filter((_, index) => index !== Delete));
+  };
+  return (
+    <div>
+      {items.map((toDoItem, index) => (
+        <>
+          <div className="list">
+            <ToDoItem item={toDoItem} key={toDoItem} />
+            <button onClick={() => handleClick(index)} className="delete-btn">
+              <FontAwesomeIcon
+                icon={faTrashCan}
+                style={{ color: "rgb(128, 63, 166)" }}
+                size="2x"
+              />
+            </button>
+          </div>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default ToDoList;
