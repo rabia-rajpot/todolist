5826593ad1c9c684a8a93bf2a81e7c10185a6c97
Rabia-Rajput <rajput.rabia@outlook.com>
2024-07-30 22:25:14 +0200

    updated file

---
+++ src/Components/Filter.js
@@ -0,0 +1,13 @@
+
+
+const Filter = () => {
+  return (
+    <div className="active">
+        <button className="btn">All</button>
+        <button className="btn">Active</button>
+        <button className="btn">Done</button>
+    </div>
+  )
+}
+
+export default Filter
