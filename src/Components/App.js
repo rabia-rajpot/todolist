e94f5f68b734a572444d071d65422de52532a2ed
Rabia-Rajput <rajput.rabia@outlook.com>
2024-07-31 23:05:26 +0200

    updated file

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ToDoInput from "./ToDoInput";
 import ToDoList from "./ToDoList";
 import "../input.css";
@@ -6,17 +6,31 @@ import Filter from "./Filter";
 
 const App = () => {
   const [items, setItems] = useState([]);
+  const [filterItems, setFilterItems] = useState("all");
 
-  const activatedItems = items?.filter(item => item.status === "active")
-  const doneItems = items?.filter(item => item.status === "done")
+  const activatedItems = items.filter(item => item.status === "active");
+  const doneItems = items.filter(item => item.status === "done");
+  
+  
+  const handleCheckbox = () => {
+    switch (filterItems) {
+      case "active":
+        return activatedItems;
+      case "done":
+        return doneItems;
+      default:
+        return items;
+    }
+  };
+  
 
-  console.log(items);
+ console.log(filterItems);
 
   return (
     <div className="main">
       <ToDoInput setItems={setItems} />
-      <Filter />
-      <ToDoList items={items} setItems={setItems} />
+      <Filter setFilterItems={setFilterItems} filterItems={filterItems} />
+      <ToDoList items={handleCheckbox()} setItems={setItems} />
     </div>
   );
 };
