1f518cacc2863042bb41d10624e201e16d8364ce
Rabia-Rajput <rajput.rabia@outlook.com>
2024-07-31 23:05:31 +0200

    updated file

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,13 +1,27 @@
-
-
-const Filter = () => {
+const Filter = ({ setFilterItems, filterItems }) => {
+    console.log(filterItems);
   return (
     <div className="active">
-        <button className="btn">All</button>
-        <button className="btn">Active</button>
-        <button className="btn">Done</button>
+      <button
+        className={`btn ${filterItems === "all" ? "filter" : ""}`}
+        onClick={() => setFilterItems("all")}
+      >
+        All
+      </button>
+      <button
+        className={`btn ${filterItems === "active" ? "filter" : ""}`}
+        onClick={() => setFilterItems("active")}
+      >
+        Active
+      </button>
+      <button
+        className={`btn ${filterItems === "done" ? "filter" : ""}`}
+        onClick={() => setFilterItems("done")}
+      >
+        Done
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
