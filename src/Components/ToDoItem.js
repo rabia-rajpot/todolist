656eaac7d28aa5191f82440ccbe5a9fb84c06c35
Rabia-Rajput <rajput.rabia@outlook.com>
2024-07-31 23:05:41 +0200

    updated file

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -9,14 +9,14 @@ const ToDoItem = ({ item, setItems }) => {
     setItems(prevItems => {
       return prevItems.map(
         prevItem => prevItem.value === item.value
-          ? {...prevItem, itemStatus: checked ? "done" : "active"}
+          ? {...prevItem, status: checked ? "done" : "active"}
           : prevItem
       )
     })
   }
   return (
     <div className="item">
-      <input onChange={handleCheck} className="box" type="checkbox" id={`checkbox-${item.value}`} />
+      <input onChange={handleCheck}  checked={item.status === "done"} className="box" type="checkbox" id={`checkbox-${item.value}`} />
       <label htmlFor={`checkbox-${item.value}`} className="custom-checkbox"></label>
       <span>{item.value}</span>
     </div>
