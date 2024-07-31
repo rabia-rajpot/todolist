5fb292fb7fa5442c6cf4feb37995b1193093bd18
Rabia-Rajput <rajput.rabia@outlook.com>
2024-07-31 11:47:47 +0200

    updated file

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "../input.css";
 
-const ToDoItem = ({ item }) => {
+const ToDoItem = ({ item, setItems }) => {
+
+  const handleCheck = (e) => {
+    const checked = e.target.checked
+
+    setItems(prevItems => {
+      return prevItems.map(
+        prevItem => prevItem.value === item.value
+          ? {...prevItem, itemStatus: checked ? "done" : "active"}
+          : prevItem
+      )
+    })
+  }
   return (
     <div className="item">
-      <input className="box" type="checkbox" id={`checkbox-${item}`} />
-      <label htmlFor={`checkbox-${item}`} className="custom-checkbox"></label>
-      <span>{item}</span>
+      <input onChange={handleCheck} className="box" type="checkbox" id={`checkbox-${item.value}`} />
+      <label htmlFor={`checkbox-${item.value}`} className="custom-checkbox"></label>
+      <span>{item.value}</span>
     </div>
   );
 };
