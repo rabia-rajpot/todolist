16571f20597e528379d51459466c5c0d4237cd8b
Rabia-Rajput <rajput.rabia@outlook.com>
2024-08-12 13:11:30 +0200

    updated file

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -2,22 +2,32 @@ import React from "react";
 import "../input.css";
 
 const ToDoItem = ({ item, setItems }) => {
-
   const handleCheck = (e) => {
-    const checked = e.target.checked
+    const checked = e.target.checked;
 
-    setItems(prevItems => {
-      return prevItems.map(
-        prevItem => prevItem.value === item.value
-          ? {...prevItem, status: checked ? "done" : "active"}
+    setItems((prevItems) => {
+      return prevItems.map((prevItem) =>
+        prevItem.id === item.id
+          ? { ...prevItem, completed: checked ? "done" : "active" }
           : prevItem
-      )
-    })
-  }
+      );
+    });
+    console.log(item.completed);
+  };
   return (
     <div className="item">
-      <input onChange={handleCheck}  checked={item.status === "done"} className="box" type="checkbox" id={`checkbox-${item.value}`} />
-      <label htmlFor={`checkbox-${item.value}`} className="custom-checkbox"></label>
+      <input
+        onChange={handleCheck}
+        checked={item.completed}
+        className="box"
+        type="checkbox"
+        id={`checkbox-${item.id}`}
+      />
+      <label
+        htmlFor={`checkbox-${item.id}`}
+        className="custom-checkbox"
+      ></label>
+      <span>{item.title}</span>
       <span>{item.value}</span>
     </div>
   );
